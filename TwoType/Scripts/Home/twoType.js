50661669145117c99665a426edb302a079e58169
slashP <perkristianhelland@gmail.com>
2014-05-13 23:48:40 +0200

    Fixed backspace and space issue when entering highscore

--- TwoType/Scripts/Home/twoType.js
+++ TwoType/Scripts/Home/twoType.js
@@ -6,6 +6,9 @@
     window.addEventListener("keydown", specialCharacterPressed);
 
     function keyPressed(e) {
+        if (typing.game().isGameFinished()) {
+            return;
+        }
         invokeKeyPressed(typing, e.which);
         if (e.which == 32) { // stop scrolling when pressing space
             e.preventDefault();
@@ -13,9 +16,12 @@
     }
 
     function specialCharacterPressed(e) {
+        if (typing.game().isGameFinished()) {
+            return;
+        }
         invokeKeyDown(typing, e.which);
         if (e.which == 8) {
             e.preventDefault();
         }
-        }
+    }
 })();
